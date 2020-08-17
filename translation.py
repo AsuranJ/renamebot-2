6384f46748be709d9992ca13ef14d5b6e40d8910
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-08-17 23:49:40 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,7 +8,7 @@ This is a Telegram Rename Bot!
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    DOWNLOAD_START = "trying to download"
+    DOWNLOAD_START = "🅣🅡🅨🅘🅝🅖 🅣🅞 🅓🅞🅦🅝🅛🅞🅐🅓 🅟🅛🅔🅐🅢🅔 🅦🅐🅘🅣"
     UPLOAD_START = "trying to upload"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.**"
@@ -26,13 +26,10 @@ This is a Telegram Rename Bot!
     
 1. Send me any Telegram File.
 2. Reply to that message to /rename new name.extension.
-   
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/yYHrSnuYXpA">Diploy</a>
 
 --------
 
-Support Group : @InFoTelGroup
-© @SpEcHlDe @TGBotsZ"""
+Support Group : @AI_BOT_HELP"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.

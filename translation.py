44ef98447f85397a43dd220f5c2344d70c781c4e
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-08-23 20:51:06 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,24 +1,27 @@
 class Translation(object):
     START_TEXT = """Hello,
 This is a simple Telegram Rename Bot!
+<b>мade вy [тнιѕ legend](https://t.me/pppppgame)</b>
 
 <b>Please send me any Telegram file and reply to that file to /rename New Name.mkv</b>
+
+<b>[Jσιɳ συɾ ƈԋαɳɳҽʅ](https://t.me/AI_bot_projects)</b>
 
 <b>/help for more details..</b>"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "𝐉𝐨𝐢𝐧 𝐨𝐮𝐫 𝐜𝐡𝐚𝐧𝐧𝐞𝐥\n  @AI_bot_projects"
-    DOWNLOAD_START = "𝓽𝓻𝔂𝓲𝓷𝓰 𝓽𝓸 𝓭𝓸𝔀𝓷𝓵𝓸𝓪𝓭 𝓹𝓵𝓮𝓪𝓼𝓮 𝔀𝓪𝓲𝓽"
-    UPLOAD_START = "𝓽𝓻𝔂𝓲𝓷𝓰 𝓽𝓸 𝓾𝓹𝓵𝓸𝓪𝓭 𝓹𝓵𝓮𝓪𝓼𝓮 𝔀𝓪𝓲𝓽"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.**\n\n[𝐉𝐨𝐢𝐧 𝐨𝐮𝐫 𝐜𝐡𝐚𝐧𝐧𝐞𝐥](https://t.me/AI_bot_projects)"
+    UPGRADE_TEXT = "Jσιɳ συɾ ƈԋαɳɳҽʅ\n  @AI_bot_projects"
+    DOWNLOAD_START = "🇹 🇷 🇾 🇮 🇳 🇬 \n🇹 🇴 \n🇩 🇴 🇼 🇳 🇱 🇴 🇦 🇩 \n🇹 🇴 \n🇸 🇪 🇷 🇻 🇪 🇷 \n🇵 🇱 🇪 🇦 🇸 🇪 \n🇼 🇦 🇮 🇹"
+    UPLOAD_START = "🇹 🇷 🇾 🇮 🇳 🇬 \n🇹 🇴 \🇺 🇵 🇱 🇴 🇦 🇩 \n🇵 🇱 🇪 🇦 🇸 🇪 \n🇼 🇦 🇮 🇹"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.**\n\n[Jσιɳ συɾ ƈԋαɳɳҽʅ](https://t.me/AI_bot_projects)"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved. This image will be used in the File."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "**Document Downloaded Successfully.**\n[𝐉𝐨𝐢𝐧 𝐨𝐮𝐫 𝐜𝐡𝐚𝐧𝐧𝐞𝐥](https://t.me/AI_bot_projects)"
+    SAVED_RECVD_DOC_FILE = "**Document Downloaded Successfully.**\n[Jσιɳ συɾ ƈԋαɳɳҽʅ](https://t.me/AI_bot_projects)"
     CUSTOM_CAPTION_UL_FILE = "renamed by\n  @TGrenamebyAI_bot"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
